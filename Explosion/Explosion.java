f92ff8bdbf6b7b00ecf1ce5d3b454e48de97859d
Russell Gordon <rgordon@lcs.on.ca>
2019-05-04 09:57:56 -0400

    Added image files

---
+++ Explosion/Explosion.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Super simple demonstration of how to create an animation in Greenfoot.
+ * 
+ * @author R. Gordon
+ * @version May 4, 2019
+ */
+public class Explosion extends Actor
+{
+    /**
+     * Constructor
+     * 
+     * When this actor is created, it makes an explosion animation.
+     * 
+     * Image files from:
+     * 
+     * http://www.bigrookgames.com/files/UndeadEmpire2DAssets.zip
+     * via
+     * https://opengameart.org/content/undeadempire-tileset-64x64-repack-floor-lava-walls-and-effects
+     */
+    Explosion()
+    {
+        
+    }
+
+    /**
+     * Act - do whatever the Explosion wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
